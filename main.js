4954dcf3e778941cf7566e2a911e0863bfba1a0b
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2022-12-21 08:30:38 +0300

    popup js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    var mobileMenu = document.querySelector('section.mobile-menu');
+    mobileMenu.style.display = 'block';
+  });
+
+ 
+
