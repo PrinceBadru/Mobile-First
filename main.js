c5115b7db3226599b0dbac6d11a5474d9906e10d
Eric Umeohabike <32669797+eric2umeh@users.noreply.github.com>
2022-12-21 09:33:44 +0100

    add close button function into the main.js

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ document.getElementById('hamburger').addEventListener('click', function() {
     mobileMenu.style.display = 'block';
   });
 
- 
+document.querySelector('.close-button').addEventListener('click', function() {
+    var mobileMenu = document.querySelector('section.mobile-menu');
+    mobileMenu.style.display = 'none';
+  });
 
