e2907e0c88261764c7fe429697c4ce4cd357620e
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2022-12-21 20:08:48 +0300

    mobile js  changes

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var mobileMenu = document.querySelector('section.mobile-menu');
-    mobileMenu.style.display = 'block';
-  });
+document.getElementById('hamburger').addEventListener('click', () => {
+  const mobileMenu = document.querySelector('section.mobile-menu');
+  mobileMenu.style.display = 'block';
+});
 
-document.querySelector('.close-button').addEventListener('click', function() {
-    var mobileMenu = document.querySelector('section.mobile-menu');
+document.querySelector('.close-button').addEventListener('click', () => {
+  const mobileMenu = document.querySelector('section.mobile-menu');
+  mobileMenu.style.display = 'none';
+});
+
+const mobileMenuOptions = document.querySelectorAll('ul.mobile-list li');
+mobileMenuOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    const mobileMenu = document.querySelector('section.mobile-menu');
     mobileMenu.style.display = 'none';
   });
+});
 
-var mobileMenuOptions = document.querySelectorAll('ul.mobile-list li');
-  mobileMenuOptions.forEach(function(option) {
-    option.addEventListener('click', function() {
-      var mobileMenu = document.querySelector('section.mobile-menu');
-      mobileMenu.style.display = 'none';
-    });
-  });
+
+
+  
+ 
+
